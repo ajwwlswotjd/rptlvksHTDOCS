089fba259e1cfc5bd0162c4f115962ced3307389
ajwwlswotjd <ajwwlswotjd@naver.com>
2019-06-01 16:22:37 +0900

    변경

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
             <div class="col-md-10 col-sm-12">
                 <div class="jumbotron bg-white" >
                     <h1 class="display-4">대충 있어보이는 사이트</h1>
-                    <p class="lead">대충 게시판이라는 뜻</p>
+                    <p class="lead">대충 게시판 이라는 뜻</p>
                     <hr class="my-4">
-                    <p>대충 뭐라도 써야할겉 같은 곳</p>
+                    <p>대충 여기도 채워야할것 같음</p>
                     <a class="btn btn-outline-primary" href="/list.php" role="button">대충 게시판 보기</a>
                     <a class="btn btn-outline-primary" href="/form.php" role="button">대충 글쓰기</a>
                 </div>
